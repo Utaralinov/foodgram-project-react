19944259779539e0e891e87a91fb74a5ae8ed42e
Nursultan Utaralinov <nutaralinov26@gmail.com>
2022-12-05 03:38:03 +0500

    Настройка сервера

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -176,6 +176,10 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'users.User'
 
 DJOSER = {
