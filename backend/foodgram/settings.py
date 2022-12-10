e5f61866736186b527926a3d155e93a0db527069
Utaralinov <92029779+Utaralinov@users.noreply.github.com>
2022-12-10 11:46:58 +0000

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='som3-s3cre1-key')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = ["*"]
 
