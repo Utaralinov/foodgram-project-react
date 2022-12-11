4e8423ea1a42ec72d3037f579be164fd957806c0
Utaralinov <92029779+Utaralinov@users.noreply.github.com>
2022-12-11 19:38:09 +0000

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -124,8 +124,8 @@ DJOSER = {
     },
 }
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_backend')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
